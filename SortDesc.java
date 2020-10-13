aad9939e3adf4f51ca7645daabf9f80aa36e4e05
grtonksj <supksj@gmail.com>
2020-10-13 22:10:05 +0900

    1problem add

---
+++ SortDesc.java
@@ -0,0 +1,27 @@
+package CodeTest;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.io.IOException;
+
+
+public class SortDesc {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    String s = br.readLine();
+    char[] arr = s.toCharArray();
+ 
+ 
+		Arrays.sort(arr);
+ 
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.print(arr[i]);
+		}
+
+
+    
+   
+  }
+}
